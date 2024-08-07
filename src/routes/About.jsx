1b5643fd5c340351e196f4972407a7a7bc92a0b7
sevarabahriddinova <sevaraxonbahriddinova@gmail.com>
2024-08-07 10:43:19 +0500

    just

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,28 +1,30 @@
-import React, { useEffect, useState } from 'react' 
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const About = () => {
- 
-  const [products,setProduct]=useState(null);
+  const [user, setUser] = useState(null);
+  const { id } = useParams();
 
-  const {id}=useParams () 
-  
-  useEffect(()=>{
-    axios(`https://reqres.in/api/users?page=2/${id}`)  
-    .then(response => setProduct (response.data.data))
-  },[])
+  useEffect(() => {
+    axios(`https://reqres.in/api/users/${id}`)
+      .then(response => setUser(response.data.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, [id]);
 
   return (
- <div>
-     {
-      products && <div>
-        <h2>{products.last_name}</h2>     
-        <h2>{products.first_name}</h2> 
-      </div>
-     }
- </div>
-    )
-}
+    <div>
+      {user ? (
+        <div className='about__wrapper'>
+          <h2>{user.first_name} {user.last_name}</h2>
+          <img className='avatarca' src={user.avatar} alt="" />
+          <p className='lorem'> Lorem ipsum dolor sit amet consectetur adipisicing elit. At deleniti quasi, maiores molestias amet non necessitatibus adipisci rerum eaque repellat alias est ea reprehenderit, velit, nihil provident vitae iusto! Quo!</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
-export default About
+export default About;
